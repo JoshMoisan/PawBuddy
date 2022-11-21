945a6368d0d5cb8e444a79a4dea4d1f0e05a218a
joshmoisan <josh.moisan@hotmail.fr>
2022-11-21 15:32:44 -0500

    booking model + association

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,4 @@
 class Dog < ApplicationRecord
+    has_many :dogs
+    belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    has_many :bookings
+    has_many :dogs
+    has_many :dogs, through: :bookings
 end
