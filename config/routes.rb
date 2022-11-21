c858a90e4121364fa3bf0f648c74ac5a11f289c7
joshmoisan <josh.moisan@hotmail.fr>
2022-11-21 16:31:49 -0500

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-end
+
+  root to: 'dogs#index'
+
+  resources :dogs do
+    resources :bookings, only: %i[new create]
+  end 
+  resources :users, only: %i[show] 
+  resources :bookings, only: %i[edit update]
+  end
