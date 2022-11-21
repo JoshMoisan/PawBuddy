69dccd4cd429c2ef5a525aa9f7d18968894d60e7
julian gaspar <juliangaspar15@gmail.com>
2022-11-21 18:02:32 +0100

    Added Dogs controller plus methods and views

---
+++ app/controllers/dogs_controller.rb
@@ -0,0 +1,20 @@
+class DogsController < ApplicationController
+  def index
+    @dogs = Dog.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
