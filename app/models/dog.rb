c24a562bf22961f74cb7e925c42ea1c3d1cd2022
julian gaspar <juliangaspar15@gmail.com>
2022-11-21 15:53:02 +0100

    Validations

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,4 +1,6 @@
 class Dog < ApplicationRecord
-    has_many :dogs
-    belongs_to :user
+  has_many :dogs
+  belongs_to :user
+  validates :name, presence: true
+  validates :description, length: { minimum: 15 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_21_140938) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_21_202154) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.string "status"
+    t.bigint "user_id", null: false
+    t.bigint "dog_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["dog_id"], name: "index_bookings_on_dog_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "dogs", force: :cascade do |t|
     t.string "name"
@@ -33,5 +45,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_21_140938) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "bookings", "dogs"
+  add_foreign_key "bookings", "users"
   add_foreign_key "dogs", "users"
 end
