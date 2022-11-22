fed2251aa5ffd4bab7ee4c300dfdabfff446ddb0
joshmoisan <josh.moisan@hotmail.fr>
2022-11-22 14:28:01 -0500

    Not-seed-butcontroller

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
-  get 'bookings/new'
-  get 'bookings/create'
-  get 'bookings/edit'
-  get 'bookings/update'
-  get 'users/show'
-  get 'dogs/index'
-  get 'dogs/show'
-  get 'dogs/new'
-  get 'dogs/create'
-  get 'dogs/edit'
-  get 'dogs/update'
+  # get 'bookings/new'
+  # get 'bookings/create'
+  # get 'bookings/edit'
+  # get 'bookings/update'
+  # get 'users/show'
+  # get 'dogs/index'
+  # get 'dogs/show'
+  # get 'dogs/new'
+  # get 'dogs/create'
+  # get 'dogs/edit'
+  # get 'dogs/update'
   devise_for :users
-  root to: 'dogs#index'
+  root to: 'pages#home'
 
   resources :dogs do
     resources :bookings, only: %i[new create]
