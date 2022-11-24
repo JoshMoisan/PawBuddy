307f23fba61d3b81b8c0a3833cc4a2e63be4272a
Dilmer Alvarado <dilmer.alvarado@gmail.com>
2022-11-24 15:58:38 +0100

    some_changes

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,5 +1,6 @@
 class Dog < ApplicationRecord
   has_many :dogs
+  has_many_attached :photos
   belongs_to :user
   validates :name, presence: true
   validates :description, length: { minimum: 15 }
